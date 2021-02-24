3abc1e7522b2fb9cf6228d641333338a2b20f58c
Joel Rausch <jsrausch.sojourn@gmail.com>
2021-02-23 21:22:12 -0600

    Refresh submodule

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include "behavior_tree/nodes/sequence_node.h"
 
 int main(int argc, char** argv) {
-    bt::RootNode root;
-    root.add_child<bt::SequenceNode>();
+    behavior_tree::RootNode root;
+    root.add_child<behavior_tree::SequenceNode>();
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
